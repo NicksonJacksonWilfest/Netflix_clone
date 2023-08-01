ef0ecf7a6f1f50dbccc49062e7af47c728f27cb1
devxjr-pro <Devxjr.pro@gmail.com>
2023-08-01 09:42:57 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function learn() {
-    document.getElementById('').innerHTML = "The information collected by Google reCAPTCHA is subject to the Google Privacy Policy and Terms of Service, and is used for providing, maintaining, and improving the reCAPTCHA service and for general security purposes (it is not used for personalized advertising by Google)."
+let details;
+document.getElementById("submit").onclick = function () {
+    details = document.getElementById("detail").value;
+    window.alert(details);
 }
